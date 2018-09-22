9853151e3eee75b1c4c2df7abf17503585e69e6f
lappe <lappe@mpib-berlin.mpg.de>
2018-09-22 22:33:08 +0200

    launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -13,21 +13,24 @@ home_dir=/home/flappix/docs/code/raspberry-looper
 
 cd $home_dir
 
+mkdir -p /tmp/llogs/
+
+dbus-launch jackd -d alsa -X raw > /tmp/llogs/jack.log 2>&1 &
 #dbus-launch jackd -R -d alsa -r 44100 -p 128 -X raw &
-#/usr/bin/jackd -dalsa -r44100 -p256 -n2 -Xraw -D -Chw:PCH -Phw:PCH,0 > logs/jack.log 2>&1 &
-/usr/bin/jackd -dalsa -r44100 -p256 -n2 -Xraw -D -Chw:CODEC -Phw:PCH,0 > logs/jack.log 2>&1 &
+#/usr/bin/jackd -dalsa -r44100 -p256 -n2 -Xraw -D -Chw:PCH -Phw:PCH,0 > /tmp/llogs/jack.log 2>&1 &
+/usr/bin/jackd -dalsa -r44100 -p256 -n2 -Xraw -D -Chw:CODEC -Phw:PCH,0 > /tmp/llogs/jack.log 2>&1 &
 sleep 1
 #rakarrack -n -b rakarrack/bank.rkrb -p 0 &
 
 # start mod-host
 mod-host -p 5555
 
-sooperlooper -l 8 -c 1 -L sooperlooper/default_session.slsess -m sooperlooper/default_midi.slb > logs/sooperlooper.log 2>&1 &
-h2cli -s hydrogen/default.h2song > logs/hydrogen.log 2>&1 &
-amsynth -x > logs/amsynth.log 2>&1 &
-fluidsynth -s -i -m jack -g 2 fluidsynth/custom.sf2 > logs/fluidsynth.log 2>&1 &
-/home/flappix/build/aubio/build/dist/usr/local/bin/aubionotes > logs/aubio.log 2>&1 & #-B 1024 -H 1024 &
-a2jmidid > logs/a2jmidid.log 2>&1 &
+sooperlooper -l 8 -c 1 -L sooperlooper/default_session.slsess -m sooperlooper/default_midi.slb > /tmp/llogs/sooperlooper.log 2>&1 &
+h2cli -s hydrogen/default.h2song > /tmp/llogs/hydrogen.log 2>&1 &
+amsynth -x > /tmp/llogs/amsynth.log 2>&1 &
+fluidsynth -s -i -m jack -g 2 fluidsynth/custom.sf2 > /tmp/llogs/fluidsynth.log 2>&1 &
+/home/flappix/build/aubio/build/dist/usr/local/bin/aubionotes > /tmp/llogs/aubio.log 2>&1 & #-B 1024 -H 1024 &
+a2jmidid > /tmp/llogs/a2jmidid.log 2>&1 &
 
 sleep 2
 
